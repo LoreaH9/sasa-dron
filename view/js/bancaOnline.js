a0eb1a80428df0be4c6597b399a815f16eee6906
LoreaH9 <l_hernandez@fpzornotza.com>
2021-11-04 11:25:00 +0100

    css

--- view/js/bancaOnline.js
+++ view/js/bancaOnline.js
@@ -1,8 +1,4 @@
 function cargar() {
-    const op = document.getElementsByClassName('btn').forEach(element => {
-      element.addEventListener("click",function() {
-        console.log(op[i].id)
-      });
-    });
+
 }    
 
