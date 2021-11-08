3927a060c2316723aea3f712ed933d0c339b6e1c
LoreaH9 <l_hernandez@fpzornotza.com>
2021-11-08 12:54:19 +0100

    logout

--- view/js/jsFunctions.js
+++ view/js/jsFunctions.js
@@ -1,18 +1,24 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-	
-	sessionVarsView();
+
+    sessionVarsView();
+
 });
-function sessionVarsView(){
-    var url="controller/cSessionVarsView.php";
-    fetch (url,{
+
+function sessionVarsView() {
+    var url = "controller/cSessionVarsView.php";
+    fetch(url, {
         method: 'GET',
-        headers:{'Content-Type': 'application/json'}
-    }).then(res => res.json()).then(result =>{
+        headers: { 'Content-Type': 'application/json' }
+    }).then(res => res.json()).then(result => {
         console.log(result);
-        if (result.user.admin==1){
+        if (result.user.admin == 1) {
             $("#btnBanca").css('display', 'block');
-          
+            $('#nomUsu').removeAttr('data-bs-target');
             $("#nomUsu").text(result.user.nombre);
+            $("#ddLg").css('display', 'none');
+            $("#ddReg").css('display', 'none');
+            $("#ddLo").css('display', 'block');
+
         }
 
     })
@@ -22,18 +28,44 @@ function login() {
     var email = $("#email").val();
     var password = $("#password").val();
 
-    var url="controller/cLogin.php";
-    var data= {'email':email, 'password':password}
+    var url = "controller/cLogin.php";
+    var data = { 'email': email, 'password': password }
 
     fetch(url, {
         method: 'POST',
         body: JSON.stringify(data),
-        headers:{'content-type': 'application/json'}
-    }).then(res => res.json()).then(result=>{
-        if (result.usuario.admin==1){
+        headers: { 'content-type': 'application/json' }
+    }).then(res => res.json()).then(result => {
+        if (result.usuario.admin == 1) {
             $("#btnBanca").css('display', 'block');
+            $("#ddLg").css('display', 'none');
+            $("#ddReg").css('display', 'none');
+            $("#ddLo").css('display', 'block');
             $('#login').modal('toggle');
+            $('#nomUsu').removeAttr('data-bs-target');
             $("#nomUsu").text(result.usuario.nombre);
         }
     })
-  }
+}
+
+function logout() {
+
+    var url = "controller/cLogout.php";
+    fetch(url, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    }).then(res => res.json()).then(result => {
+        console.log(result);
+        if (result.error == "no error") {
+            $("#btnBanca").css('display', 'none');
+            $("#ddLg").css('display', 'block');
+            $("#ddReg").css('display', 'block');
+            $("#ddLo").css('display', 'none');
+            $("#nomUsu").text("Login");
+            $('#nomUsu').attr('data-bs-target', '#login');
+            $("#email").val("");
+            $("#password").val("");
+        }
+
+    })
+}
