b81e92571c8cbe1d7bbcdc2046cda4fd4e625ee4
LoreaH9 <l_hernandez@fpzornotza.com>
2021-11-09 12:21:43 +0100

    nav

--- view/js/bancaOnline.js
+++ view/js/bancaOnline.js
@@ -1,4 +1,63 @@
-function cargar() {
 
-}    
+$(document).ready(init);
+
+function init() {
+    option();
+}
+
+function option() {
+
+    $('#movimientos')
+        .off('click')
+        .on('click', movimientos);
+    $('#leasing')
+        .off('click')
+        .on('click', leasing);
+    $('#prestamos')
+        .off('click')
+        .on('click', prestamos);
+    $('#transferencias')
+        .off('click')
+        .on('click', tranferencias);
+    $('#ingreso')
+        .off('click')
+        .on('click', ingreso);
+
+}
+function preventClick(event) {
+    event.preventDefault();
+    event.stopPropagation();
+}
+
+function movimientos(event) {
+    preventClick(event);
+    $("#tableMovimientos").show();
+    console.log("m")
+}
+function leasing(event) {
+    preventClick(event);
+    console.log("leasing")
+
+    $("#tableMovimientos").hide();
+}
+function prestamos(event) {
+    preventClick(event);
+    console.log("prestamos")
+
+    $("#tableMovimientos").hide();
+}
+function tranferencias(event) {
+    preventClick(event);
+    console.log("tranferencias")
+
+    $("#tableMovimientos").hide();
+}
+function ingreso(event) {
+    preventClick(event);
+    console.log("ingreso")
+
+    $("#tableMovimientos").hide();
+}
+
+
 
