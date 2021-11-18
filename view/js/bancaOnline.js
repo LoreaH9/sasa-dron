d1052b2a63887fb394fc4ae15e40fc143677bd84
kevino333 <74492727+kevino333@users.noreply.github.com>
2021-11-18 10:43:20 +0100

    Datos movimientos
    Se recogen los datos de la BBDD de movimientos  y se insertan en una tabla

--- view/js/bancaOnline.js
+++ view/js/bancaOnline.js
@@ -24,12 +24,43 @@ function hideAllTables() {
     $("#tableMovimientosCuentaCredito").hide();
     $("#tableLeasing").hide();
     $("#tableMovimientosCuentaCorriente").hide();
+	$("#tableMovimientos").hide();
 }
 
 function showOption(event) {
     preventClick(event);
     hideAllTables();
     $("#table"+event.currentTarget.id+"").show();
+	$("#tableMovimientos").click(tableMovimientosList());
+}
+
+function tableMovimientosList(){
+	console.log("Llega a movList");
+	var url ="controller/cMovimientos.php";
+	fetch(url, {
+	  method: 'POST', // or 'POST'
+	  //body: JSON.stringify(data), // data can be `string` or {object}!
+	  //headers:{'Content-Type': 'application/json'}  //input data	  
+	}).then(res => res.json()).then(result => {
+		console.log(result);
+		
+		var movimientos = result.list;
+		var newRow = "";
+		for(let k=0; k<movimientos.length; k++){
+		newRow +="<tr class='table-danger'>"+"<td>"+movimientos[k].id+"</td>"
+							+"<td>"+movimientos[k].fecha+"</td>"
+							+"<td>"+movimientos[k].fechaValor+"</td>"
+							+"<td>"+movimientos[k].concepto+"</td>"
+							+"<td>"+movimientos[k].importe+"</td>"
+							+"<td>"+movimientos[k].saldo+"</td>"
+							+"<td>"+movimientos[k].dias+"</td>"
+							+"<td>"+movimientos[k].idUsuario+"</td>"
+							+"<td>"+movimientos[k].idCredito+"</td>"
+							+"<td>"+movimientos[k].idCorriente+"</td>"
+						+"</tr>"
+		}
+		$("#tableMovBody").html(newRow);
+	}).catch(error => console.error('Error status:', error));	   
 }
 
 
