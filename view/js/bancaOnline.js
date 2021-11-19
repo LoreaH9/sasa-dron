28f2f509c6e8bde69c37897d0a960ed75fc8da2d
kevino333 <74492727+kevino333@users.noreply.github.com>
2021-11-19 09:46:36 +0100

    Cambiar donde se muestran los movimientos (sin acabar)
    Los movimientos tienen que aparecer en movimientos cuentas credito, no en movimientos

--- view/js/bancaOnline.js
+++ view/js/bancaOnline.js
@@ -24,12 +24,65 @@ function hideAllTables() {
     $("#tableMovimientosCuentaCredito").hide();
     $("#tableLeasing").hide();
     $("#tableMovimientosCuentaCorriente").hide();
+	$("#tableMovimientos").hide();
 }
 
 function showOption(event) {
     preventClick(event);
     hideAllTables();
     $("#table"+event.currentTarget.id+"").show();
+	$("#MovimientosCuentaCredito").click(tableMovimientosList());
+}
+
+function tableMovimientosList(){
+	console.log("Llega a movList");
+	var url ="controller/cMovimientos.php";
+	fetch(url, {
+	  method: 'POST', // or 'POST'
+	  //body: JSON.stringify(data), // data can be `string` or {object}!
+	  //headers:{'Content-Type': 'application/json'}  //input data	  
+	}).then(res => res.json()).then(result => {
+		console.log(result);
+		
+		var movimientos = result.list;
+		movimientos.add("acreedor");//hartzekoduna
+		movimientos.add("deudor");//zorduna
+		
+		var newRow = "";
+		for(let k=0; k<movimientos.length; k++){
+//		newRow +="<tr class='table-danger'>"+"<td>"+movimientos[k].id+"</td>"
+//							+"<td>"+movimientos[k].fecha+"</td>"
+//							+"<td>"+movimientos[k].fechaValor+"</td>"
+//							+"<td>"+movimientos[k].concepto+"</td>"
+//							+"<td>"+movimientos[k].importe+"</td>"
+//							+"<td>"+movimientos[k].saldo+"</td>"
+//							+"<td>"+movimientos[k].dias+"</td>"
+//							+"<td>"+movimientos[k].idUsuario+"</td>"
+//							+"<td>"+movimientos[k].idCredito+"</td>"
+//							+"<td>"+movimientos[k].idCorriente+"</td>"
+//						+"</tr>"
+		if(k>0 && movimientos[k].saldo > movimientos[(k-1)].saldo){
+			movimientos[k].acreedor = movimientos[k].saldo - movimientos[(k-1)].saldo;
+		}
+
+		newRow +="<tr class='table-danger'>"+"<td>"+movimientos[k].id+"</td>"
+							+"<td>"+movimientos[k].fecha+"</td>"
+							+"<td>"+movimientos[k].fechaValor+"</td>"
+							+"<td>"+movimientos[k].concepto+"</td>"
+							+"<td>"+movimientos[k].importe+"</td>"
+							+"<td>"+movimientos[k].acreedor+"</td>"//añadir artzekoduna
+							+"<td>"+movimientos[k].saldo+"</td>"
+							+"<td>"+movimientos[k].dias+"</td>"
+							+"<td>"+movimientos[k].importe+"</td>"//zorduna
+							//artzekoduna
+							//gaineratikoa
+//							+"<td>"+movimientos[k].idUsuario+"</td>"
+//							+"<td>"+movimientos[k].idCredito+"</td>"
+//							+"<td>"+movimientos[k].idCorriente+"</td>"
+						+"</tr>"
+		}
+		$("#tableMovCreBody").html(newRow);
+	}).catch(error => console.error('Error status:', error));	   
 }
 
 
