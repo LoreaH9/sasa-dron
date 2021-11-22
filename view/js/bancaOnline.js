ddfce363a7a91979e456bd59247b2d230654420f
kevino333 <74492727+kevino333@users.noreply.github.com>
2021-11-22 13:13:52 +0100

    Movimientos cuentas de credito
    - El botón Movimientos cuenta corriente ha sido eliminado
    + Recoge movimientos de la BBDD
    + Distingue cuando el saldo esta siendo agregado o retirado
    + Distingue cuando el saldo es inferior a un limite
    + Las líneas con saldo negativo aparecen en rojo

--- view/js/bancaOnline.js
+++ view/js/bancaOnline.js
@@ -4,7 +4,9 @@ $(document).ready(init);
 function init() {
     option();
     hideAllTables();
-    $("#tableMovimientosCuenta").show();
+	tableMovimientosList();
+	$("#tableMovimientosCuentaCredito").show();
+	
 }
 
 function option() {
@@ -23,15 +25,15 @@ function hideAllTables() {
     $("#tablePrestamos").hide();
     $("#tableMovimientosCuenta").hide();
     $("#tableLeasing").hide();
-    //$("#tableMovimientosCuentaCorriente").hide();
-	//$("#tableMovimientos").hide();
+    $("#tableMovimientosCuentaCredito").hide();
+	$("#tableMovimientos").hide();
 }
 
 function showOption(event) {
     preventClick(event);
     hideAllTables();
     $("#table"+event.currentTarget.id+"").show();
-	$("#MovimientosCuentaCredito").click(tableMovimientosList());
+	console.log("Boton clicado: " + event.currentTarget.id);
 }
 
 function tableMovimientosList(){
@@ -45,40 +47,74 @@ function tableMovimientosList(){
 		console.log(result);
 		
 		var movimientos = result.list;
-		movimientos.add("acreedor");//hartzekoduna
-		movimientos.add("deudor");//zorduna
-		
+		movimientos[0]["acreedor"];//hartzekoduna
+		movimientos[0]["deudor"];//zorduna
+		movimientos[0]["movAcreedor"];//segunda columna hartzekoduna
+		movimientos[0]["movDeudor"];//segunda columna zorduna
+		movimientos[0]["resto"];//gaineratikoa
+		console.log(movimientos);
 		var newRow = "";
 		for(let k=0; k<movimientos.length; k++){
-//		newRow +="<tr class='table-danger'>"+"<td>"+movimientos[k].id+"</td>"
-//							+"<td>"+movimientos[k].fecha+"</td>"
-//							+"<td>"+movimientos[k].fechaValor+"</td>"
-//							+"<td>"+movimientos[k].concepto+"</td>"
-//							+"<td>"+movimientos[k].importe+"</td>"
-//							+"<td>"+movimientos[k].saldo+"</td>"
-//							+"<td>"+movimientos[k].dias+"</td>"
-//							+"<td>"+movimientos[k].idUsuario+"</td>"
-//							+"<td>"+movimientos[k].idCredito+"</td>"
-//							+"<td>"+movimientos[k].idCorriente+"</td>"
-//						+"</tr>"
-		if(k>0 && movimientos[k].saldo > movimientos[(k-1)].saldo){
-			movimientos[k].acreedor = movimientos[k].saldo - movimientos[(k-1)].saldo;
-		}
+//		if(k>0 && parseInt(movimientos[k].saldo) > parseInt(movimientos[(k-1)].saldo)){
+//			console.log(movimientos[k].id + ": Se suma saldo");
+//			movimientos[k].acreedor = (parseInt(movimientos[k].saldo) - parseInt(movimientos[(k-1)].saldo))+".000";
+//			movimientos[k].deudor = "";
+//		} else if (k>0 && parseInt(movimientos[k].saldo) < parseInt(movimientos[(k-1)].saldo)){
+//			console.log(movimientos[k].id + ": Se resta saldo");
+//			movimientos[k].deudor = (parseInt(movimientos[(k-1)].saldo) - parseInt(movimientos[k].saldo))+".000";
+//			movimientos[k].acreedor = "";
+//		} else {
+//			console.log(movimientos[k].id + ": Se mantiene el saldo")
+//			movimientos[k].deudor = "";
+//			movimientos[k].acreedor = "";
+//		}
 
-		newRow +="<tr class='table-danger'>"+"<td>"+movimientos[k].id+"</td>"
-							+"<td>"+movimientos[k].fecha+"</td>"
+		if(movimientos[k].importe.includes("-")){ 			//Se resta saldo
+			console.log(movimientos[k].id + ": Se resta saldo");
+		
+			movimientos[k].deudor = movimientos[k].importe;
+			movimientos[k].acreedor = "";
+			movimientos[k].movDeudor = movimientos[k].saldo+"x"+movimientos[k].dias
+			movimientos[k].movAcreedor = "";
+		} else if (!movimientos[k].importe.includes("-")){	//Se suma saldo
+			console.log(movimientos[k].id + ": Se suma saldo");
+		
+			movimientos[k].acreedor = movimientos[k].importe;
+			movimientos[k].deudor = "";
+			movimientos[k].movAcreedor = movimientos[k].saldo+"x"+movimientos[k].dias
+			movimientos[k].movDeudor = "";
+		} else { 											//Se mantiene saldo
+			console.log(movimientos[k].id + ": Se mantiene saldo");
+		
+			movimientos[k].acreedor = "";
+			movimientos[k].deudor = "";
+		}
+		
+		if (parseInt(movimientos[k].saldo) < -40000){
+			movimientos[k].resto = parseInt(movimientos[k].saldo) - (-40000);
+			movimientos[k].resto *= -1; //Convierte numeros negativos en positivos
+		} else {
+			movimientos[k].resto = "";
+		}
+		
+		if (parseInt(movimientos[k].saldo) < 0){
+			movimientos[k].saldo += " Z"
+			newRow +="<tr class='table-danger'>"+"<th scope='row'>"+movimientos[k].id+"</th>"	
+		} else {
+			movimientos[k].saldo += " H"
+			newRow +="<tr class='table'>"+"<th scope='row'>"+movimientos[k].id+"</td>"
+		}
+			newRow +=
+							 "<td>"+movimientos[k].fecha+"</td>"
 							+"<td>"+movimientos[k].fechaValor+"</td>"
 							+"<td>"+movimientos[k].concepto+"</td>"
-							+"<td>"+movimientos[k].importe+"</td>"
-							+"<td>"+movimientos[k].acreedor+"</td>"//a�adir artzekoduna
+							+"<td>"+movimientos[k].deudor+"</td>"//zorduna
+							+"<td>"+movimientos[k].acreedor+"</td>"//artzekoduna
 							+"<td>"+movimientos[k].saldo+"</td>"
 							+"<td>"+movimientos[k].dias+"</td>"
-							+"<td>"+movimientos[k].importe+"</td>"//zorduna
-							//artzekoduna
-							//gaineratikoa
-//							+"<td>"+movimientos[k].idUsuario+"</td>"
-//							+"<td>"+movimientos[k].idCredito+"</td>"
-//							+"<td>"+movimientos[k].idCorriente+"</td>"
+							+"<td>"+movimientos[k].movDeudor+"</td>"//zorduna, saldo y dias
+							+"<td>"+movimientos[k].movAcreedor+"</td>"//artzekoduna, saldo y dias
+							+"<td>"+movimientos[k].resto+"</td>"//gaineratikoa, si el saldo es negativo, por cuanto
 						+"</tr>"
 		}
 		$("#tableMovCreBody").html(newRow);
